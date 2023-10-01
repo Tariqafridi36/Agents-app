c5d2cb0e631589e288ee6b9d5e76e9d2e8e4cb5d
Tariq Iqbal <tariqafridi36@gmail.com>
2023-10-01 11:42:43 +0400

    MVC model

--- QueueManager.cs
+++ QueueManager.cs
@@ -1,11 +1,11 @@
 // Install the RabbitMQ.Client NuGet package
-
-using RabbitMQ.Client;
 using System.Text;
+using RabbitMQ.Client;
 
 public class QueueManager
 {
     private readonly IConnection _connection;
+
     private readonly IModel _channel;
 
     public QueueManager()
@@ -13,13 +13,22 @@ public class QueueManager
         var factory = new ConnectionFactory() { HostName = "localhost" };
         _connection = factory.CreateConnection();
         _channel = _connection.CreateModel();
-        _channel.QueueDeclare(queue: "chat_sessions", durable: false, exclusive: false, autoDelete: false, arguments: null);
+        _channel
+            .QueueDeclare(queue: "chat_sessions",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
     }
 
     public void EnqueueChatSession(string message)
     {
         var body = Encoding.UTF8.GetBytes(message);
-        _channel.BasicPublish(exchange: "", routingKey: "chat_sessions", basicProperties: null, body: body);
+        _channel
+            .BasicPublish(exchange: "",
+            routingKey: "chat_sessions",
+            basicProperties: null,
+            body: body);
     }
 
     public void Close()
